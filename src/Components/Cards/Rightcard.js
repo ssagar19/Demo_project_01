7784f54619eac204254066c84bdc5c6ea59cb467
ssagar19 <sagarshubham47@gmail.com>
2021-05-06 08:46:31 +0530

    initial commit

---
+++ src/Components/Cards/Rightcard.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Leftcard() {
+  return (
+    <div className="leftCard">
+      <h1>Related To This Course</h1>
+      <h2>Classroom</h2>
+      <h3>Class Files</h3>
+    </div>
+  );
+}
+
+export default Leftcard;
